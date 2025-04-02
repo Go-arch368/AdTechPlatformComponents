89886e545037b630e1bb34865b8b65d388f13153
Gowtham T <gowthamtj0808@gmail.com>
2025-04-02 18:48:36 +0530

    debugging

--- components/ui/input.stories.tsx
+++ components/ui/input.stories.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Calendar as CalendarIcon } from "lucide-react";
-
+import { format } from "date-fns";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 
@@ -106,7 +106,7 @@ const DateInputComponent = ({ disabled, size }: { disabled?: boolean; size?: "de
             )}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
-          
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0">
