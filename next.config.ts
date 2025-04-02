493a72d87e8a00c819f43128c3471086e0ecf1d1
Gowtham T <gowthamtj0808@gmail.com>
2025-04-02 22:32:52 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,10 @@
-import { NextConfig } from 'next';
-
-const config: NextConfig = {
+export default {
   async rewrites() {
     return [
       {
         source: "/storybook/:path*",
-        destination: "/storybook-static/:path*", // serves Storybook from the static directory
-      },
-    ];
-  },
-  // Optionally, add public directory for assets if required
-  async headers() {
-    return [
-      {
-        source: "/storybook-static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable", // cache Storybook assets for a long time
-          },
-        ],
+        destination: "/storybook-static/:path*",
       },
     ];
   },
 };
-
-export default config;
